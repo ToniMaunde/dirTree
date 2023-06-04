fa0869b6472db048f371281a3640d1f73044de17
ToniMaunde <maundemilton@gmail.com>
2023-06-04 11:59:10 +0200

    feat: add icons

---
+++ src/assets/icons/logo.ts
@@ -0,0 +1,14 @@
+import type { TIcon } from "../../components/Icon";
+
+const logoIcon: TIcon = {
+  viewBox: "0 0 24 24",
+  paths: [
+    {
+      d: "M8 2H4V6H5V22H7V18H16V19H20V15H16V16H7V12H16V13H20V9H16V10H7V6H8V2Z"
+    },
+  ],
+  title: "logo",
+  id: "logo"
+};
+
+export default logoIcon;
